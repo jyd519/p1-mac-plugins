2b9ad0e7f37224ca8eb6e635d301ac3c9684b07f
Stéphan Kochen <stephan@kochen.nl>
2015-02-01 18:28:00 +0100

    Update for p1stream changes.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+var _ = require('underscore');
+var native = require('./build/Release/native.node');
+
 module.exports = function(scope) {
-    var native = require('./build/Release/native.node');
+    // Set config defaults for `root:mac-sources` before init.
+    scope.$on('preInit', function() {
+        var settings = scope.cfg['root:mac-sources'] ||
+            (scope.cfg['root:mac-sources'] = {});
+        _.defaults(settings, {
+            type: 'root:mac-sources'
+        });
+    });
 
-    var pdata = scope.data.p.p1_mac_sources = {};
-
-    var monitor = new native.DetectDisplays({
-        onChange: function(list) {
-            pdata.displays = list;
-            scope.$mark();
-        }
+    // Set detected displays on the root.
+    scope.o.$onCreate('root:mac-sources', function(obj) {
+        obj.$monitor = new native.DetectDisplays({
+            onChange: function(list) {
+                obj.displays = list;
+                obj.$mark();
+            }
+        });
     });
 };
