cb697ffc603aeb08d63b2fef7faea83677bc98c2
Stéphan Kochen <stephan@kochen.nl>
2015-02-02 00:24:57 +0100

    Implement object types for audio/video capture.

--- index.js
+++ index.js
@@ -7,15 +7,75 @@ module.exports = function(scope) {
         var settings = scope.cfg['root:p1-mac-sources'] ||
             (scope.cfg['root:p1-mac-sources'] = {});
         _.defaults(settings, {
-            type: 'root:p1-mac-sources'
+            type: 'root:p1-mac-sources',
+            audioQueueIds: [],
+            displayStreamIds: []
         });
     });
 
-    // Set detected displays on the root.
+    // Define the plugin root type.
     scope.o.$onCreate('root:p1-mac-sources', function(obj) {
+        obj.$resolveAll('audioQueues');
+        obj.$resolveAll('displayStreams');
+
+        // Set detected displays on the root.
         obj.$monitor = new native.DetectDisplays({
             onChange: function(list) {
                 obj.displays = list;
+                obj.$mark();
+            }
+        });
+    });
+
+    // Implement audio queue source type.
+    scope.o.$onCreate('source:audio:p1-mac-sources:audio-queue', function(obj) {
+        obj.$activation({
+            start: function() {
+                try {
+                    obj.$instance = new native.AudioQueue({
+                        device: obj.cfg.device
+                    });
+                }
+                catch (err) {
+                    // FIXME: log error
+                    obj.hasError = true;
+                }
+                obj.$mark();
+            },
+            stop: function() {
+                obj.$instance.destroy();
+                obj.$instance = null;
+                obj.$mark();
+            }
+        });
+    });
+
+    // Implement display stream source type.
+    scope.o.$onCreate('source:video:p1-mac-sources:display-stream', function(obj) {
+        obj.$activation({
+            cond: function() {
+                // In addition to the default condition, ensure the display is
+                // detected before we activate the stream.
+                return obj.$defaultCond() &&
+                    _.findWhere(scope.o['root:p1-mac-sources'].displays, {
+                        displayId: obj.cfg.displayId
+                    });
+            },
+            start: function() {
+                try {
+                    obj.$instance = new native.DisplayStream({
+                        displayId: obj.cfg.displayId
+                    });
+                }
+                catch (err) {
+                    // FIXME: log error
+                    obj.hasError = true;
+                }
+                obj.$mark();
+            },
+            stop: function() {
+                obj.$instance.destroy();
+                obj.$instance = null;
                 obj.$mark();
             }
         });
