e907c4551d3c736a94282f0ecf0767d30b8417cf
Stéphan Kochen <stephan@kochen.nl>
2015-03-29 21:04:40 +0200

    Update for p1stream changes.

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var native = require('./build/Release/native.node');
 
 var previewServiceName = "com.p1stream.P1stream.preview";
 
-module.exports = function(scope) {
+module.exports = function(app) {
     // Set config defaults for `root:p1-mac-plugins` before init.
-    scope.$on('preInit', function() {
-        var settings = scope.cfg['root:p1-mac-plugins'] ||
-            (scope.cfg['root:p1-mac-plugins'] = {});
+    app.on('preInit', function() {
+        var settings = app.cfg['root:p1-mac-plugins'] ||
+            (app.cfg['root:p1-mac-plugins'] = {});
         _.defaults(settings, {
             type: 'root:p1-mac-plugins',
             audioQueueIds: [],
@@ -16,46 +16,48 @@ module.exports = function(scope) {
     });
 
     // Define the plugin root type.
-    scope.o.$onCreate('root:p1-mac-plugins', function(obj) {
-        obj.$resolveAll('audioQueues');
-        obj.$resolveAll('displayStreams');
+    app.store.onCreate('root:p1-mac-plugins', function(obj) {
+        obj.resolveAll('audioQueues');
+        obj.resolveAll('displayStreams');
 
         // Set detected displays on the root.
-        obj.$monitor = new native.DetectDisplays({
+        obj._detectDisplays = new native.DetectDisplays({
             onEvent: function(id, arg) {
                 switch (id) {
                     case native.EV_DISPLAYS_CHANGED:
                         obj.displays = arg;
-                        obj.$mark();
-                        obj.$log.info("Updated displays, %d active", arg.length);
+                        app.mark();
+
+                        obj._log.info("Updated displays, %d active", arg.length);
                         break;
 
                     default:
-                        scope.handleNativeEvent(obj, id, arg);
+                        obj.handleNativeEvent(obj, id, arg);
                         break;
                 }
             }
         });
 
         // Set detected audio inputs on the root.
-        obj.$monitor = new native.DetectAudioInputs({
+        obj._detectAudioInputs = new native.DetectAudioInputs({
             onEvent: function(id, arg) {
                 switch (id) {
                     case native.EV_AUDIO_INPUTS_CHANGED:
                         obj.audioInputs = arg;
-                        obj.$mark();
-                        obj.$log.info("Updated audio inputs, %d active", arg.length);
+                        app.mark();
+
+                        obj._log.info("Updated audio inputs, %d active", arg.length);
                         break;
 
                     default:
-                        scope.handleNativeEvent(obj, id, arg);
+                        obj.handleNativeEvent(obj, id, arg);
                         break;
                 }
             }
         });
 
         // Handle preview connections.
-        obj.$log.info("Registering preview service '%s'", previewServiceName);
+        obj._log.info("Registering preview service '%s'", previewServiceName);
         native.startPreviewService({
             name: previewServiceName,
             onEvent: function(id, arg) {
@@ -65,26 +67,24 @@ module.exports = function(scope) {
                         break;
 
                     default:
-                        scope.handleNativeEvent(obj, id, arg);
+                        obj.handleNativeEvent(obj, id, arg);
                         break;
                 }
             }
         });
         function connectHook(hook) {
-            var id = hook.mixerId;
-
-            var mixer = id && id[0] !== '$' && scope.o[id];
+            var mixer = app.o(hook.mixerId);
             if (!mixer || mixer.cfg.type !== 'mixer') {
-                obj.$log.info("Preview request for invalid mixer '%s'", id);
+                obj._log.info("Preview request for invalid mixer '%s'", hook.mixerId);
                 return hook.destroy();
             }
 
-            obj.$log.info("Adding preview hook for mixer '%s'", id);
+            obj._log.info("Adding preview hook for mixer '%s'", hook.mixerId);
 
             hook.onClose = destroy;
-            var cancel = mixer.$addFrameListener({
-                hook: hook,
-                $destroy: destroy
+            var cancel = mixer.addFrameListener({
+                videoHook: hook,
+                destroy: destroy
             });
 
             function destroy() {
@@ -95,13 +95,13 @@ module.exports = function(scope) {
     });
 
     // Implement audio queue source type.
-    scope.o.$onCreate('source:audio:p1-mac-plugins:audio-queue', function(obj) {
-        obj.$activation({
+    app.store.onCreate('source:audio:p1-mac-plugins:audio-queue', function(obj) {
+        obj.activation('native audio queue', {
             cond: function() {
                 // In addition to the default condition, ensure the input is
                 // detected before we activate the source.
-                return obj.$defaultCond() &&
-                    _.findWhere(scope.o['root:p1-mac-plugins'].audioInputs, {
+                return obj.defaultCond() &&
+                    _.findWhere(app.o('root:p1-mac-plugins').audioInputs, {
                         deviceId: obj.cfg.deviceId
                     });
             },
@@ -115,88 +115,89 @@ module.exports = function(scope) {
                     });
                 }
                 catch (err) {
-                    return obj.$fatal(err, "Failed to instantiate AudioQueue");
+                    return obj.fatal(err, "Failed to instantiate AudioQueue");
                 }
 
-                obj.$instance = inst;
-                obj.$mark();
+                obj._instance = inst;
+                app.mark();
 
                 function onEvent(id, arg) {
                     switch (id) {
                         case native.EV_AQ_IS_RUNNING:
                             if (arg) {
-                                obj.$log.info('Capture started');
+                                obj._log.info('Capture started');
                             }
                             else {
-                                if (obj.$instance === inst) {
-                                    obj.$fatal('Capture unexpectedly stopped');
-                                    obj.$instance = null;
+                                if (obj._instance === inst) {
+                                    obj.fatal('Capture unexpectedly stopped');
+                                    obj._instance = null;
+                                    app.mark();
                                 }
                                 else {
-                                    obj.$log.info('Capture stopped');
+                                    obj._log.info('Capture stopped');
                                 }
                                 inst.destroy();
                             }
                             break;
                         default:
-                            scope.handleNativeEvent(obj, id, arg);
+                            obj.handleNativeEvent(obj, id, arg);
                             break;
                     }
                 }
             },
             stop: function() {
-                if (obj.$instance) {
-                    obj.$instance.stop();
-                    obj.$instance = null;
+                if (obj._instance) {
+                    obj._instance.stop();
+                    obj._instance = null;
                 }
-                obj.$mark();
+                app.mark();
             }
         });
     });
 
     // Implement display stream source type.
-    scope.o.$onCreate('source:video:p1-mac-plugins:display-stream', function(obj) {
-        obj.$activation({
+    app.store.onCreate('source:video:p1-mac-plugins:display-stream', function(obj) {
+        obj.activation('native display stream', {
             cond: function() {
                 // In addition to the default condition, ensure the display is
                 // detected before we activate the stream.
-                return obj.$defaultCond() &&
-                    _.findWhere(scope.o['root:p1-mac-plugins'].displays, {
+                return obj.defaultCond() &&
+                    _.findWhere(app.o('root:p1-mac-plugins').displays, {
                         displayId: obj.cfg.displayId
                     });
             },
             start: function() {
                 try {
-                    obj.$instance = new native.DisplayStream({
+                    obj._instance = new native.DisplayStream({
                         displayId: obj.cfg.displayId,
                         onEvent: function(id, arg) {
-                            scope.handleNativeEvent(obj, id, arg);
+                            obj.handleNativeEvent(obj, id, arg);
                         }
                     });
                 }
                 catch (err) {
-                    return obj.$fatal(err, "Failed to instantiate DisplayStream");
+                    return obj.fatal(err, "Failed to instantiate DisplayStream");
                 }
-                obj.$mark();
+                app.mark();
             },
             stop: function() {
-                if (obj.$instance) {
-                    obj.$instance.destroy();
-                    obj.$instance = null;
+                if (obj._instance) {
+                    obj._instance.destroy();
+                    obj._instance = null;
                 }
-                obj.$mark();
+                app.mark();
             }
         });
     });
 
     // Implement display clock type.
-    scope.o.$onCreate('clock:p1-mac-plugins:display-link', function(obj) {
-        obj.$activation({
+    app.store.onCreate('clock:p1-mac-plugins:display-link', function(obj) {
+        obj.activation('native display link', {
             cond: function() {
                 // In addition to the default condition, ensure the display is
                 // detected before we activate the clock.
-                return obj.$defaultCond() &&
-                    _.findWhere(scope.o['root:p1-mac-plugins'].displays, {
+                return obj.defaultCond() &&
+                    _.findWhere(app.o('root:p1-mac-plugins').displays, {
                         displayId: obj.cfg.displayId
                     });
             },
@@ -205,21 +206,20 @@ module.exports = function(scope) {
                     obj.$instance = new native.DisplayLink({
                         displayId: obj.cfg.displayId,
                         onEvent: function(id, arg) {
-                            scope.handleNativeEvent(obj, id, arg);
+                            obj.handleNativeEvent(id, arg);
                         }
                     });
                 }
                 catch (err) {
-                    return obj.$fatal(err, "Failed to instantiate DisplayLink");
+                    return obj.fatal(err, "Failed to instantiate DisplayLink");
                 }
-                obj.$mark();
             },
             stop: function() {
-                if (obj.$instance) {
-                    obj.$instance.destroy();
-                    obj.$instance = null;
+                if (obj._instance) {
+                    obj._instance.destroy();
+                    obj._instance = null;
                 }
-                obj.$mark();
+                app.mark();
             }
         });
     });
